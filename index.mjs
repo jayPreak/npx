2b1164dc12ea2a3333f2d987c692cb3ca007d862
jayPreak <jayesh.preak2003@gmail.com>
2023-04-18 15:24:32 +0530

    he got the dawg in himmm (pink)

--- index.mjs
+++ index.mjs
@@ -1,13 +1,16 @@
 import chalk from "chalk";
 import boxen from "boxen";
 
+const pink = chalk.hex("#F0B2BC");
+// const gitblack = chalk.hex("#0D1117");
+
 const card = `
 ${chalk.bold("Jayesh Bhushan")} / ${chalk.gray("jaypreak")}
 ${chalk.gray("Full Stack Developer ")}
 
-${chalk.gray("Email:")} ${chalk.pink("jayesh.preak2003@gmail.com")}
-${chalk.gray("GitHub:")} ${chalk.cyan("https://github.com/jaypreak")}
-${chalk.gray("LinkedIn:")} ${chalk.cyan(
+${chalk.gray("Email:")} ${pink("jayesh.preak2003@gmail.com")}
+${chalk.gray("GitHub:")} ${pink("https://github.com/jaypreak")}
+${chalk.gray("LinkedIn:")} ${pink(
   "https://www.linkedin.com/in/jayesh-bhushan-587616200/"
 )}
 `;
